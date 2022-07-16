f2a00e7551c7244e68ef2063bd16595a88f8ab27
Gabriel <Gabrielpichane74@gmail.com>
2022-07-16 10:29:38 -0300

    1.5
    Signed-off-by: Gabriel <Gabrielpichane74@gmail.com>

--- app/src/main/java/estudo/produto/trabalhoempreendedorismo/Fragments/PortuguesFragment/PtNivel1Fragment.kt
+++ app/src/main/java/estudo/produto/trabalhoempreendedorismo/Fragments/PortuguesFragment/PtNivel1Fragment.kt
@@ -1,13 +1,17 @@
 package estudo.produto.trabalhoempreendedorismo.Fragments.PortuguesFragment
 
+import android.net.MailTo.parse
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import estudo.produto.trabalhoempreendedorismo.R
 import estudo.produto.trabalhoempreendedorismo.databinding.FragmentPtNivel1Binding
+import java.net.HttpCookie.parse
 import java.net.URL
+import java.util.logging.Level.parse
 
 
 class PtNivel1Fragment : Fragment() {
@@ -19,13 +23,21 @@ class PtNivel1Fragment : Fragment() {
 
         binding.btVideoPt1.setOnClickListener {
 
-         URL("https://www.youtube.com/results?search_query=aulas+basicas+de+portugues").toURI()
+            obras()
+
+               URL("https://www.youtube.com/results?search_query=aulas+basicas+de+portugues")
 
         }
 
+
     }
 
+  private fun obras(){
 
+      Toast.makeText(activity,"ESTAMOS EM OBRAS! VOLTE MAIS TARDE!", Toast.LENGTH_SHORT).show()
+
+
+  }
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {return binding.root}
